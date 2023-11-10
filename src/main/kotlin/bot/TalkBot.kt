ca4044cc7552119c5a682b011e1b37ee33de7e9a
iostyle <developerlty@gmail.com>
2023-11-10 17:16:18 +0800

    feat: support coroutines

--- src/main/kotlin/bot/TalkBot.kt
+++ src/main/kotlin/bot/TalkBot.kt
@@ -1,5 +1,8 @@
 package bot
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
@@ -17,31 +20,33 @@ class TalkBot : TelegramLongPollingBot() {
     }
 
     override fun onUpdateReceived(update: Update) {
-        System.out.println("${update.message}")
-        if (update.hasMessage() && update.message.hasText()) {
-            if (update.message.isSuperGroupMessage || update.message.isGroupMessage
-                || update.message.isChannelMessage
-            ) {
-                System.out.println("${update.message.chat.type}")
-                return
-            }
-            val message = SendMessage() // Create a SendMessage object with mandatory fields
-            message.chatId = update.message.chatId.toString()
+        GlobalScope.launch(Dispatchers.IO) {
+            System.out.println("${update.message}")
+            if (update.hasMessage() && update.message.hasText()) {
+                if (update.message.isSuperGroupMessage || update.message.isGroupMessage
+                        || update.message.isChannelMessage
+                ) {
+                    System.out.println("${update.message.chat.type}")
+                    return@launch
+                }
+                val message = SendMessage() // Create a SendMessage object with mandatory fields
+                message.chatId = update.message.chatId.toString()
 
-            val result = update.message.text
-                .replace("@functionzzbot", "")
-                .replace("你", "_Wo_")
-                .replace("我", "你")
-                .replace("_Wo_", "我")
-                .replace("吗", "")
-                .replace("吧", "")
-                .replace("？", "")
+                val result = update.message.text
+                        .replace("@functionzzbot", "")
+                        .replace("你", "_Wo_")
+                        .replace("我", "你")
+                        .replace("_Wo_", "我")
+                        .replace("吗", "")
+                        .replace("吧", "")
+                        .replace("？", "")
 
-            message.text = result
-            try {
-                execute(message) // Call method to send the message
-            } catch (e: TelegramApiException) {
-                e.printStackTrace()
+                message.text = result
+                try {
+                    execute(message) // Call method to send the message
+                } catch (e: TelegramApiException) {
+                    e.printStackTrace()
+                }
             }
         }
     }
