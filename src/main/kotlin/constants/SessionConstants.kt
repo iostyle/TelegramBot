8b980e5611b0e9e1c6b8791f2fb0af619f5b6b07
iostyle <developerlty@gmail.com>
2023-11-10 18:45:10 +0800

    feat: looper

---
+++ src/main/kotlin/constants/SessionConstants.kt
@@ -0,0 +1,6 @@
+package constants
+
+object SessionConstants {
+    const val CHAT_ID_0 = ""
+    const val CHAT_ID_1 = ""
+}
--- src/main/kotlin/launch/Launcher.kt
+++ src/main/kotlin/launch/Launcher.kt
@@ -1,6 +1,12 @@
 package launch
 
 import bot.TalkBot
+import constants.SessionConstants
+import extension.BotExtension.sendMessage
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
@@ -8,4 +14,15 @@ fun main() {
     val api = TelegramBotsApi(DefaultBotSession::class.java)
     val bot = TalkBot()
     api.registerBot(bot)
+
+//    looper(bot)
+}
+
+fun looper(bot: TalkBot) {
+    GlobalScope.launch {
+        while (isActive){
+            delay(1000)
+            bot.sendMessage(SessionConstants.CHAT_ID_1,"Timer: ${System.currentTimeMillis()}")
+        }
+    }
 }
